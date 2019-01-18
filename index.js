b7292404410d2d6f02db6b602286e00c1c5be61a
EC2 Default User <ec2-user@ip-172-31-5-96.us-east-2.compute.internal>
2019-01-18 01:30:41 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var SlackBot = require("slackbots");
+var randomWords = require('random-words');
+var config = require('./config/development.json')
+var alc = require("./alc.js")
+var channel = "hubot-dev";
+var map = {}; 
+ 
+var bot = new SlackBot({
+   token: config.yutsbot.token,
+   name: "yutsbot"
+});
+
+bot.on("start", function() {
+    console.log("Hello world!");
+});
+
+bot.on("message", function(data) {
+    if (data.type !== "message") {
+        return;
+    }
+    
+    handleMessage(data);
+});
+
+function handleMessage(message) {
+    switch(message.text) {
+        case "plzq":
+            sendWord(message.user);
+            break;
+        case "plza":
+            sendMeaning(message.user);
+            break;
+        case "ping":
+            bot.postMessage(message.channel, "PONG!");
+            break;
+        default:
+            return;
+    }
+}
+
+function sendWord(user){
+    if (map[user]) {
+        bot.postMessageToChannel(channel, "<@" + user + "> さんの今日の単語はすでに登録されています「" + map[user] + "」です。");
+        return;
+    }
+    map[user] = randomWords();
+    bot.postMessageToChannel(channel, "<@" + user + "> さんの今日の単語は「" + map[user] + "」です。");
+    return;
+}
+
+function sendMeaning(user) {
+    if(!map[user]){
+        bot.postMessageToChannel(channel, "まだ今日の単語が登録されていません");
+        return;
+    }
+    var result = alc(map[user], bot);
+    delete map[user];
+}
